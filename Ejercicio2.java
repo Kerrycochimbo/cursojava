6bbccd996bafc7060d655471fc313c766113cae5
Kerrycochimbo <jhonwick1999op@gmail.com>
2025-10-14 18:11:35 -0500

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package secuenciales;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      
+        double primerReto = 3.5;
+        double segundoReto = 4.2;
+        double tercerReto = 2.8;
+        double promedio;
+
+        // Cálculo del promedio
+        promedio = (primerReto + segundoReto + tercerReto) / 3;
+
+        
+       
+        System.out.println("Tu promedio final es " + promedio + " - Nivel Superado");
+    }
+}
+
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ ejercicio1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package secuenciales;
+
+/**
+ *
+ * @author Juan Pablo Franco
+ */
+public class ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    String username="camila";    
+     int Cofreuno=8;
+     int Cofredos=12;
+     int SumaDulces;
+     
+     SumaDulces=Cofreuno + Cofredos;
+     
+     System.out.print(username + "Camila ahora tiene un total de "+ SumaDulces +"  caramelos magicos");
+     
+     
+     
+     
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
